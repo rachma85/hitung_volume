160e97e382f2c53328eab95640d80177cb04758d
rachma85 <120188967+rachma85@users.noreply.github.com>
2025-02-27 15:17:51 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,9 @@ st.title("Aplikasi Menghitung Volume")
 st.write(
     "Aplikasi ini digunakan untuk menghitung volume benda antara lain: bola, tabung, kerucut, prisma, balok, kubus."
 )
+option = st.selectbox(
+    "Volume apa yang akan dihitung?",
+    ("Bola", "Tabung", "Kerucut","Prisma","Balok","Kubus"),
+)
+
+st.write("You selected:", option)
